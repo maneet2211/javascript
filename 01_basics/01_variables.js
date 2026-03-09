baaad98c69b9ca174b925dc42dbddc0c8e650316
maneet2211 <maneetkohli12@gmail.com>
2026-03-09 18:36:13 +0000

    done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = "123456"
+let accountEmail = "maneet@google.com"
+var accountPassword = "123456789"
+
+accountCity = "jalandhar"
+
+
+
+// step1
+// console.log(accountID);
+// console.log(accountEmail);
+// console.log(accountPassword);     
+// console.log(accountCity);
+
+// step2
+console.log([accountId, accountEmail, accountPassword, accountCity]);
+
+
+
